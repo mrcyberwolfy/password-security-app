71f8182ae8c6a922022b94dfb89d09c1a4912147
mrcyberwolfy <imcyberwolfy@gmail.com>
2025-05-07 00:31:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import json
-import hashlib
 
 app = Flask(__name__)
 
@@ -12,28 +11,17 @@ def index():
     score = None
 
     if request.method == 'POST':
-        password = request.form.get('password')
         zxcvbn_result = request.form.get('zxcvbn_result')
 
-        if password and zxcvbn_result:
+        if zxcvbn_result:
             try:
                 zxcvbn_data = json.loads(zxcvbn_result)
                 entropy = zxcvbn_data.get('entropy')
                 score = zxcvbn_data.get('score')
                 crack_times = zxcvbn_data.get('crack_times_display')
                 feedback = zxcvbn_data.get('feedback')
-
-                hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-                with open("data.txt", "a") as file:
-                    file.write(f"Hash: {hashed_password}\n")
-                    file.write(f"Entropy: {entropy}\n")
-                    file.write(f"Score: {score}\n")
-                    file.write(f"Suggestions: {feedback.get('suggestions', [])}\n")
-                    file.write(f"---\n")
-
             except json.JSONDecodeError:
-                feedback = {"warning": "Could not analyze the password. Invalid data."}
+                feedback = {"warning": "Gesla ni bilo mogoče analizirati. Neveljavni podatki."}
 
     return render_template('index.html', crack_times=crack_times, feedback=feedback, entropy=entropy, score=score)
 
